ad6ae5e91bcd2c6e1d14ba59210c64b9a6a98b16
kenway214 <kenway214@outlook.com>
2024-05-20 16:19:55 +0200

    marble: Project Pixel-ify
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -7,17 +7,28 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common aosp configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
-PRODUCT_DEVICE := marble
-PRODUCT_MANUFACTURER := Xiaomi
+# Device identifier
 PRODUCT_BRAND := POCO
+PRODUCT_DEVICE := marble
 PRODUCT_MODEL := 23049PCD8G
+PRODUCT_NAME := aosp_marble
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := marble_global
 PRODUCT_SYSTEM_DEVICE := marble
+
+# PixelPlus OS
+TARGET_SUPPORTS_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_QUICK_TAP := true
+CUSTOM_BUILD_TYPE := Official
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.8.0.UMREUXM release-keys" \
